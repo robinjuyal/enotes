79f93a826a6a9ef33ec91e7306f96c355283a387
robinjuyal <rjsoi.mad1@gmail.com>
2024-10-21 09:38:33 +0530

    first commit

---
+++ src/main/java/com/rj/Enotes_API_Service/EnotesApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rj.Enotes_API_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnotesApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnotesApiServiceApplication.class, args);
+	}
+
+}
